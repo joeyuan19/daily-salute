9340fc45ba3514a50feec944e29a0b251f357550
Joe Yuan <Joe.Yuan19@gmail.com>
2014-09-22 11:32:02 -0400

    first commit

---
+++ server.py
@@ -0,0 +1,38 @@
+import os.path
+
+import tornado.httpserver
+import tornado.ioloop
+import tornado.options
+import tornado.web
+
+from tornado.options import define, options
+
+
+define("port", default=15210, help="run on the given port", type=int)
+
+
+def load_index():
+    with open('index.html','r') as f:
+        content = ''.join(i for i in f)
+    return content
+
+
+
+class IndexHandler(tornado.web.RequestHandler):
+    def get(self):
+        self.write(load_index())
+
+if __name__ == "__main__":
+    tornado.options.parse_command_line()
+
+    app = tornado.web.Application(
+        [
+            (r'/', IndexHandler)  
+        ],
+    )
+    http_server = tornado.httpserver.HTTPServer(app)
+    http_server.listen(options.port)
+    tornado.ioloop.IOLoop.instance().start()
+
+
+
